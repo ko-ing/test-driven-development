4a965361ec083d7d954d48a060643643250b890c
ko-ing <318921@naver.com>
2021-04-06 17:55:58 +0900

    test-1

--- src/test/java/com/testdrivendevelopment/tdd/TddApplicationTests.java
+++ src/test/java/com/testdrivendevelopment/tdd/TddApplicationTests.java
@@ -2,6 +2,7 @@ package com.testdrivendevelopment.tdd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TddApplicationTests {
@@ -10,4 +11,14 @@ class TddApplicationTests {
     void contextLoads() {
     }
 
+    public void testMultiplication() {
+        Dollar five =  new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
 }
+
+//TODO:
+// $5 + 10CHF = $10 (환율이 2:1일 경우)
+// $5 * 2 = $10
